3e981b5689681fb8e212bd34f7ac7c8bc996c8fd
RicardoQC <ricardoqc@byzoop.com>
2024-04-18 10:32:55 -0500

    Se actualizo los archivos index y controller.

--- src/controller.js
+++ src/controller.js
@@ -12,6 +12,26 @@ export const getProducts = async (req, res) => {
   }
 }
 
-export const getProductById = (req, res) => {
-  res.send('Mostrando producto con el id ...')
+export const getProductById = async (req, res) => {
+  try {
+    const { id } = req.params
+
+    // Validar que el parámetro id sea un número
+    const idNumber = parseInt(id)
+    if (isNaN(idNumber)) {
+      return res.status(400).json({ message: 'El id debe ser un número' })
+    }
+
+    // Traer el producto de la base de datos
+    const [producto] = await pool.execute('SELECT * FROM product WHERE product_id=?', [idNumber])
+
+    // Validar que exista el producto
+    if (producto.length === 0) {
+      return res.status(404).json({ message: 'Producto no encontrado' })
+    }
+
+    res.json(producto[0])
+  } catch (error) {
+    res.status(500).json({ message: 'Hubo un error interno', detail: error.message })
+  }
 }
